e27b13e67049a15143ceba1ab13732f1f200f598
Pranav Chaganty <45394613+xtreme5351@users.noreply.github.com>
2022-08-18 00:46:12 +0100

    Add files via upload
    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SudokuSolver
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[,] inputBoard = { };
+            _ = new Solver(inputBoard);
+        }
+    }
+}
---
+++ Solver.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace SudokuSolver
+{
+    public class Solver
+    {
+        private int[,] board;
+        private int boardLength;
+        private int rowIndex = 0;
+        private int colIndex = 0;
+
+        public Solver(int[,] inputBoard)
+        {
+            board = inputBoard;
+            boardLength = board.Length;
+            Solve();
+        }
+
+        private void Solve()
+        {
+            for (int row = 0; row < boardLength; row++)
+            {
+                for (int col = 0; col < boardLength; col++)
+                {
+
+                }
+            }
+        }
+    }
+}
